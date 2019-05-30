fd8497ab4bebd96307e4e7f8a9eda4c9102c6011
Nour <nour.kh.9345@gmail.com>
2019-05-31 00:03:08 +0300

    Add basic user and articles modules with their routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {UserGuardService} from '@services/user-guard/user-guard.service';
+import {ArticlesGuardService} from '@services/articles-guard/articles-guard.service';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'articles'
+  },
+  {
+    path: 'user',
+    loadChildren: './user/user.module#UserModule',
+    canLoad: [UserGuardService]
+  },
+  {
+    path: 'articles',
+    loadChildren: './articles/articles.module#ArticlesModule',
+    canLoad: [ArticlesGuardService]
+  }
 ];
 
 @NgModule({
